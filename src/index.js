861f7108ec725478b7a460648ef6df37949d60b4
jyc-coder <jyc4648@gmail.com>
2022-09-28 16:32:38 +0900

    #6 - 코드 수정
    헤로쿠 배포문제를 해결함. 
    
    https://stackoverflow.com/questions/73374091/heroku-rails-react-eresolve-could-not-resolve
    
    를 참고하여 heroku에 배포하려는 app의 setting에서  `config var`의 key값으로  `NPM_CONFIG_LEGACY_PEER_DEPS`을 `true`로 추가해주고 배포하니 성공적으로 동작하는 것을 확인했다.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import './index.css';
 import App from './App';
 import { RecoilRoot } from 'recoil';
 
-const root = document.getElementById('root');
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <RecoilRoot>
       <App />
     </RecoilRoot>
   </React.StrictMode>,
-  root,
 );
