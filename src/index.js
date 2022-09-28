919877929a12f3b8d4ae3a6a9b14453622f1110d
jyc-coder <jyc4648@gmail.com>
2022-09-28 16:03:08 +0900

    #6 - 오류 발생으로 인한 코드 수정
    ReactDOM 의 import 경로에 /client가 달라붙어있어서 수정하고 
    react17버전에 맞는 index.js코드로 변경하여 제대로 동작함을 확인함

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { RecoilRoot } from 'recoil';
 
+const root = document.getElementById('root');
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
       <App />
     </RecoilRoot>
   </React.StrictMode>,
+  root,
 );
